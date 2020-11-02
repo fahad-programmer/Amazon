89a88b2bde076184a29214144f3617b2d07f523f
fahadmalik-creator <unexpectedprogrammer@gmail.com>
2020-11-03 02:37:58 +0500

    Done Some Setting To The FrontEnd As Main Will Be Done By @imran-prog Using the js for slider

---
+++ static/home/js/slider.js
@@ -0,0 +1,36 @@
+let i = 0;
+
+var images = [];
+var time = 5000; //Time After Which You Want the picture to change
+
+//Images List
+images[0] =
+  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_es_US_2x._CB429092343_.jpg";
+images[1] =
+  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_es_US_2x._CB428980068_.jpg";
+images[2] =
+  "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_es_US_1x._CB432534552_.jpg";
+
+images[3] =
+  "https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg";
+
+//Slider Logic
+// Change Image
+function changeImg() {
+  document.slider.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
