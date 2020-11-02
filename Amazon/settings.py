c726c295e83e0f9ccbade7a4ee3b6306196b7d80
fahadmalik-creator <unexpectedprogrammer@gmail.com>
2020-11-03 00:04:11 +0500

    Header Done

--- Amazon/settings.py
+++ Amazon/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('This is index Page')
+    return render(request, 'home/index.html')
