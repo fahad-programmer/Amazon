80b0746ef5061d8e29860ef0fac973b39efdef76
fahadmalik-creator <unexpectedprogrammer@gmail.com>
2020-11-11 06:58:08 +0500

    Changed The Index Page (Filter Out Some Product And Showed Them On Front Page). Frontend Remaining Assigned To @imran-prog

--- Shop/models.py
+++ Shop/models.py
@@ -5,10 +5,11 @@ from django.db import models
 
 class Product(models.Model):
     id = models.AutoField
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
     category = models.CharField(max_length=50)
     sub_category = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
+    rating = models.IntegerField(default=1)
     price = models.FloatField()
     image = models.ImageField(upload_to="Shop/images")
     pub_date = models.DateField()
